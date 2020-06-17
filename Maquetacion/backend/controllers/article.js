4d4584d9b6f1a4616894b67e2098607572158360
Arthuro Ali Gomez <64677836+Emocrat3@users.noreply.github.com>
2020-06-17 19:35:07 -0400

    Metodos creados que saca todos los articulos y sacar los ultimos articulos por limite

--- Maquetacion/backend/controllers/article.js
+++ Maquetacion/backend/controllers/article.js
@@ -1,4 +1,7 @@
 'use strict'
+var validator = require('validator');
+var Article = require('../models/article');
+const article = require('../models/article');
 
 var controller = {
 
@@ -17,7 +20,96 @@ var controller = {
        return res.status(200).send({
            message: 'Soy la accion test de mi controlador de articulos'
        });
-   }
+   },
+
+   save: (req,res) => {
+       // Recoger parametro por post
+        var params = req.body;
+
+
+       // validar datos con el validator
+
+       try{
+           var validate_title = !validator.isEmpty(params.title);
+           var validate_content = !validator.isEmpty(params.content);
+
+
+       }catch(err){
+            return res.status(200).send({
+                message: 'Faltan datos por enviar!!!'
+            });
+       }
+
+       if(validate_title && validate_content){
+
+       
+       // Crear el objeto a guardar
+        var article = new Article();
+
+       // Asignar valores
+       article.title = params.title;
+       article.content = params.content;
+       article.image = null;
+
+       // Guardar el articulo
+        article.save((err, articleStored) =>{
+            if(err || !articleStored){
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'El articulo no se ha guardado'
+                });
+
+            }
+
+            // Devolver una respuesta 
+
+            return res.status(200).send({
+                status: 'success',
+                article: articleStored
+            });
+            
+        });
+
+   }else{
+       return res.status(200).send({
+        status: 'error',
+        message: 'Los datos no son validos!!!'
+       });
+    }
+}, 
+
+    getArticles: (req,res) => {
+
+        var query = Article.find({});
+
+        var last = req.params.last;
+        if(last || last != undefined){
+            query.limit(5);
+        }
+
+        // find 
+        query.sort('-_id').exec((err,articles) => {
+        
+        if(err){
+            return res.status(500).send({
+                status: 'error',
+                message: 'Error al devolver los articulos'
+            });
+        }
+        if(!articles){
+            return res.status(404).send({
+                status: 'error',
+                message: 'No hay articulo para mostrar'
+            });
+        }
+        
+        return res.status(200).send({
+            status: 'sucess',
+            articles
+        });
+
+    });
+}
 
 }; // End controller 
 
--- Maquetacion/backend/routes/article.js
+++ Maquetacion/backend/routes/article.js
@@ -4,8 +4,15 @@ var express = require('express');
 var ArticleController = require('../controllers/article');
 
 var router = express.Router();
+// Rutas de prueba
 
 router.get('/test-de-controlador', ArticleController.test);
 router.post('/datos-curso', ArticleController.datosCurso);
+
+// Rutas utiles
+
+router.post('/save', ArticleController.save);
+router.get('/articles/:last?', ArticleController.getArticles);
+
 
 module.exports = router;
