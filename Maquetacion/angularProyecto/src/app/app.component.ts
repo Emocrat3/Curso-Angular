edf0192e05b2019f51a235a95e24f9e670731f44
Arthuro Ali Gomez <64677836+Emocrat3@users.noreply.github.com>
2020-06-20 18:59:12 -0400

    Modelos, comunicacion entre componentes y contenido variable en el slider

--- Maquetacion/angularProyecto/src/app/app.component.ts
+++ Maquetacion/angularProyecto/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularProyecto';
+  public title = 'Curso de Angular de Arthuro Gomez';
+  
 }
--- Maquetacion/angularProyecto/src/app/components/peliculas/peliculas.component.ts
+++ Maquetacion/angularProyecto/src/app/components/peliculas/peliculas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pelicula } from '../../models/pelicula';
 
 @Component({
   selector: 'app-peliculas',
@@ -6,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./peliculas.component.css']
 })
 export class PeliculasComponent implements OnInit {
-public peliculas: Array<any>;
+public peliculas: Pelicula[];
   constructor() { 
     this.peliculas = [
-      {year: 2015, title: "Camino ninja", image: "http://4.bp.blogspot.com/-v9T9Py3Sezo/UBIi7mwOYWI/AAAAAAAAGuA/aAanCiV6fFk/s1600/Naruto+the+Movie+6+Road+to+Ninja.jpg"},
-      {year: 2016, title: "Prisioneros del cielo", image: "https://i.playview.io/images/2019/01/08/5xInMEyCa4mZoAgpxAfmH1kG3GM.jpg"},
-      {year: 2017, title: "El arco y la flecha escarlatas", image: "https://2.bp.blogspot.com/-O3_zYH_GyDc/V6rdAw_rpkI/AAAAAAAAC8M/fKKER4p2y4EI7wTSJNckh-dyWMlPfTaQwCLcB/s1600/28s4i06.png"}
+      new Pelicula("Camino ninja", 2015, "http://4.bp.blogspot.com/-v9T9Py3Sezo/UBIi7mwOYWI/AAAAAAAAGuA/aAanCiV6fFk/s1600/Naruto+the+Movie+6+Road+to+Ninja.jpg" ),
+      new Pelicula("Camino ninja", 2016, "https://i.playview.io/images/2019/01/08/5xInMEyCa4mZoAgpxAfmH1kG3GM.jpg"),
+      new Pelicula("El arco y la flecha escarlatas", 2017, "https://2.bp.blogspot.com/-O3_zYH_GyDc/V6rdAw_rpkI/AAAAAAAAC8M/fKKER4p2y4EI7wTSJNckh-dyWMlPfTaQwCLcB/s1600/28s4i06.png")
    ];
   }
 
