7cf1ae6af0587ba8c74def51f2a05489e7268ead
Arthuro Ali Gomez <64677836+Emocrat3@users.noreply.github.com>
2020-06-19 23:31:58 -0400

    Aplicando directivas (ngIf, ngFor, ngStyle, ngClass y ngTemplate

--- Maquetacion/angularProyecto/src/app/components/peliculas/peliculas.component.ts
+++ Maquetacion/angularProyecto/src/app/components/peliculas/peliculas.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./peliculas.component.css']
 })
 export class PeliculasComponent implements OnInit {
+public peliculas: Array<any>;
+  constructor() { 
+    this.peliculas = [
+      {year: 2015, title: "Camino ninja", image: "http://4.bp.blogspot.com/-v9T9Py3Sezo/UBIi7mwOYWI/AAAAAAAAGuA/aAanCiV6fFk/s1600/Naruto+the+Movie+6+Road+to+Ninja.jpg"},
+      {year: 2016, title: "Prisioneros del cielo", image: "https://i.playview.io/images/2019/01/08/5xInMEyCa4mZoAgpxAfmH1kG3GM.jpg"},
+      {year: 2017, title: "El arco y la flecha escarlatas", image: "https://2.bp.blogspot.com/-O3_zYH_GyDc/V6rdAw_rpkI/AAAAAAAAC8M/fKKER4p2y4EI7wTSJNckh-dyWMlPfTaQwCLcB/s1600/28s4i06.png"}
+   ];
+  }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    console.log(this.peliculas);
   }
 
 }
