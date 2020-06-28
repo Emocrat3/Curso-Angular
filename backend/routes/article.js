79ee758bb4d959c223500777b70abce9e3d7b035
Arthuro Ali Gomez <64677836+Emocrat3@users.noreply.github.com>
2020-06-28 16:00:01 -0400

    probando heroku 8

--- backend/routes/article.js
+++ backend/routes/article.js
@@ -12,7 +12,9 @@ router.get('/test-de-controlador', ArticleController.test);
 router.post('/datos-curso', ArticleController.datosCurso);
 
 // Rutas utiles
-
+app.get('/', (request, response) => {
+    return response.send('Ping!');
+    });
 router.post('/save', ArticleController.save);
 router.get('/articles/:last?', ArticleController.getArticles);
 router.get('/article/:id', ArticleController.getArticle);
