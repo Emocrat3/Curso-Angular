1cc4dd8ac19907422c40e7a14abbda0c66756292
Arthuro Ali Gomez <64677836+Emocrat3@users.noreply.github.com>
2020-06-29 15:49:01 -0400

    Heroku mostrando frontend

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,8 @@
 
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var path = require('path');
+
+
 // Ejecutar express (http)
 
 
@@ -19,26 +19,15 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 // CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
 
 
 
 
 // Añadir prefijos a rutas / Cargar rutas
 
-app.use('/api', require('./routes/article'));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+
 
 // Exportar modulo (fichero  actual)
 module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 'use strict'
 
 var mongoose = require('mongoose');
-var app = require('./app');
+var express = require('express');
 var port = process.env.PORT || 3900;
-
-
-
+var bodyParser = require('body-parser');
+var app = express();
+var path = require('path');
+var ArticleController = require('./controllers/article');
+var multipart = require('connect-multiparty');
+var router = require('./routes/article');
+var md_upload = multipart({uploadDir: './upload/articles'});
+var cors = require('cors');
+app.use(cors());
+    
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
@@ -20,6 +27,22 @@ mongoose.connect('mongodb://arthuroali:Ali140116@ds113522.mlab.com:13522/heroku_
                 console.log('Servidor corriendo en http://localhost:'+port);
         });
 });
+app.use('/api', router);
 
 
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/', ArticleController.probar);
+app.post('/save', ArticleController.save);
+app.get('/articles/:last?', ArticleController.getArticles);
+app.get('/article/:id', ArticleController.getArticle);
+app.put('/article/:id', ArticleController.update);
+app.delete('/article/:id', ArticleController.delete);
+app.post('/upload-image/:id?', md_upload , ArticleController.upload);
+app.get('/get-image/:image', ArticleController.getImage);
+app.get('/search/:search', ArticleController.search);
