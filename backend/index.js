9119353f49a224c29ecccdd3b2ced0943665f17b
Arthuro Ali Gomez <64677836+Emocrat3@users.noreply.github.com>
2020-07-03 22:04:45 -0400

    Funcionando

--- backend/index.js
+++ backend/index.js
@@ -36,9 +36,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res)=>{
-        res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+
 
 app.get('/api/', ArticleController.probar);
 app.post('/api/save', ArticleController.save);
